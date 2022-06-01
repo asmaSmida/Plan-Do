a3404be63bc00a3fd065096fb4a81e798970df87
Emna Cheniour <emnacheniour@insat.u-carthage.tn>
2022-06-02 00:27:10 +0100

    updates

--- src/components/whoarewe/Whoarewe.jsx
+++ src/components/whoarewe/Whoarewe.jsx
@@ -9,7 +9,7 @@ export default function Whoarewe() {
         <div className={classes.whoarewetext}>
             <h1>Le 1er site de location des maisons d'hôtes pour vos évènements en Tunisie !</h1>
             <br />
-            <hr></hr>
+            <hr className={classes.hrwho}></hr>
             <br />
             <p>Filtrez par type de logement, nombre d’invités et durée du séjour. Notre site Web vous proposera une liste de résultats basés sur vos besoins. Vous pouvez voir les photos, les prix les avis des précédents locataires et plus. Une fois convaincus, réservez directement via notre plateforme et payez en toute sécurité.</p>
         </div>
